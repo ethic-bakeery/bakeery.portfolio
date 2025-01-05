4673de1f5cfdfbacb2a701e0ce77f8dab6aefb81
bakeery <bakeery@vbox.analyst>
2025-01-05 17:43:30 +0530

    fix

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -6,16 +6,10 @@ const GlobeComponent = () => {
   let mapContainer: HTMLDivElement | undefined;
 
   const visitedCountries = [
-    "France",
-    "China",
-    "Italy",
-    "Sri Lanka",
-    "Turkey",
-    "Greece",
-    "Malta",
-    "Hungary",
-    "Portugal",
-    "Marocco",
+    "India",
+    "Cameroon",
+    "Ethiopia",
+    "Nigeria",
   ];
 
   onMount(() => {
