c7e3a905fae706a51ac275d3201ed0f3071319b9
bakeery <bakeery@vbox.analyst>
2025-01-05 17:08:12 +0530

    intial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const LINKS = {
+  github: "https://github.com/ethic-bakeery",
+  linkedin: "https://www.linkedin.com/in/abubakar-usman-011115263/",
+  medium: "https://medium.com/@bakeery",
+  telegram: "t.me/b3keery",
+  email: "ethic.bakeery@gmail.com",
+  blog: "https://bakeery-blog-seven.vercel.app/"
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
